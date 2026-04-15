854fb66875832fef774f3c0f3cf91576f2d93251
Montserrat Carrera Leal <a01713040@tec.mx>
2026-04-14 19:05:04 -0600

    Add MixColumns implementation for AES
    Implement MixColumns function for AES encryption, including GF(2^8) multiplication.

--- paso4_mixColumns.py
+++ paso4_mixColumns.py
@@ -1,2 +1,40 @@
-def function mixColumns(bloque):
-    return bloque
+# Implementacion de MixColumns de AES
+
+# Multiplicación en GF(2^8)
+
+def mul2(x):
+    res = x << 1
+    if x & 0x80:      
+        res ^= 0x1b   
+    return res & 0xff 
+
+
+def mul3(x):
+    return mul2(x) ^ x 
+
+#Mix Columns 
+
+def mixcolumns(bloque): 
+
+    nuevo = bloque.copy()
+
+    # recorrer columnas 
+    for i in range(0, 16, 4):
+        a0 = bloque[i]
+        a1 = bloque[i+1]
+        a2 = bloque[i+2]
+        a3 = bloque[i+3]
+
+        # mix columns
+        b0 = mul2(a0) ^ mul3(a1) ^ a2 ^ a3
+        b1 = a0 ^ mul2(a1) ^ mul3(a2) ^ a3
+        b2 = a0 ^ a1 ^ mul2(a2) ^ mul3(a3)
+        b3 = mul3(a0) ^ a1 ^ a2 ^ mul2(a3)
+
+        # Guardar los resultados
+        nuevo[i]   = b0
+        nuevo[i+1] = b1
+        nuevo[i+2] = b2
+        nuevo[i+3] = b3
+
+    return nuevo
